261005772673c507b7ccf9fade3402672210a156
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-02 21:44:52 -0600

    Made change to the input container.  Was previously wrapped in a div tag, switched to a form tag with action = submit.  Now the input field event listener is triggered by both the enter key and clicking the add button;

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const input = document.getElementById('task-input');
 const addBtn = document.getElementById('add-btn');
 const taskList = document.getElementById('task-list');
-
+const empty = document.getElementById('empty-list');
 
 // Add task function
 const addTask = (e) => {
-  e.preventDefault;
+  e.preventDefault();
+// Remove "empty" message when TASK is created
+  empty.classList.add('remove');
+
 
 // Create the li element which will be the actual task
   const task = document.createElement('li');
   task.innerText = input.value;
   task.classList.add('task');
+  // Append the  taskList / task to the DOM
   taskList.appendChild(task);
 // Create the complete / delete buttons
   
-// Append the  taskList / task to the DOM
-  
+//Clear the input field after the TASK has been created
+  input.value = '';  
 }
-
-
 
 // Event Listeners
 addBtn.addEventListener('click', addTask);
-addBtn.addEventListener('keydown',function(e){
-  if (13 == e.key) {
-  console.log("enter enter");
-}  
-});
+// addBtn.addEventListener('keydown',function(e){
+//   if (13 == e.key) {
+//   console.log("enter enter");
+// }  
+// });
